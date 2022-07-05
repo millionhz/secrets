54d2370cbcc57c12425b777a9088c1168e069199
millionhz <m.waqas.hamza@gmail.com>
2022-07-05 17:34:27 +0500

    implement login errors

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const email = form.elements[0];
+const password = form.elements[1];
+
+const alert = document.querySelector('.alert');
+
+function submitForm() {
+  const data = new URLSearchParams();
+  data.set(email.name, email.value);
+  data.set(password.name, password.value);
+
+  fetch('/login', {
+    method: form.method,
+    body: data,
+  }).then((res) => {
+    if (res.url === window.location.href) {
+      alert.style.display = null;
+    } else {
+      window.location.href = res.url;
+    }
+  });
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  submitForm();
+});
