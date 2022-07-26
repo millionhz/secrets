7b3db19df706f8a13a1495606e0f29be1815f653
millionhz <m.waqas.hamza@gmail.com>
2022-07-26 21:21:43 +0500

    fix submit error handling

--- routes/submit.js
+++ routes/submit.js
@@ -8,13 +8,18 @@ router.get('/', ensureLoggedIn('/login'), (req, res) => {
   res.render('submit');
 });
 
-router.post('/', ensureLoggedIn('/login'), (req, res) => {
+router.post('/', ensureLoggedIn('/login'), (req, res, next) => {
   const { secret } = req.body;
   const { id } = req.user;
 
-  Secret.addSecret(id, secret).finally(() => {
-    res.redirect('/secrets');
-  });
+  Secret.addSecret(id, secret)
+    .then(() => {
+      res.redirect('/secrets');
+    })
+    .catch((err) => {
+      res.redirect('/secrets');
+      next(err);
+    });
 });
 
 module.exports = router;
